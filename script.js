1ecd877b793976aa388380f5a978c4a65e3a067f
William <wms274@nyu.edu>
2022-08-20 17:51:36 -0400

    background image added and input functionality improved

--- script.js
+++ script.js
@@ -2,16 +2,38 @@
 const log = document.getElementById('values');
 //let input = document.querySelector('input').value;
 
-/*function showMeWhatISaid() { 
-    log.textContent = input;
-    alert(input);
-}*/
-
-const textBox = document.querySelector('input');
+/*const textBox = document.querySelector('input');
 textBox.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     let input = document.querySelector('input').value;
     log.textContent += `${input}, `
     alert(input);
   }
+});
+const button = document.querySelector('button');
+button.addEventListener("click", function() {
+    let input = document.querySelector('input').value;
+    log.textContent += `${input}, `
+    alert(input);
+});*/
+
+const textBox = document.querySelector('input');
+textBox.addEventListener("keypress", function(event) {
+    let input = document.querySelector('input').value;
+    if (event.key === "Enter") {
+        if (input.length > 0 && input.length<=5) {
+            log.textContent += `${input}, `;
+        } else {
+            alert('Guess must contain at least one and no greater than 5 elements');
+        }
+    }   
+});
+const button = document.querySelector('button');
+button.addEventListener("click", function() {
+    let input = document.querySelector('input').value;
+    if (input.length > 0 && input.length<=5) {
+        log.textContent += `${input}, `;
+    } else {
+        alert('Guess must contain at least one and no greater than 5 elements');
+    }
 });
