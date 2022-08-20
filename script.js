506ae02af99042afe53f457f5e358cb185dedc9c
William <wms274@nyu.edu>
2022-08-20 14:15:09 -0400

    Adding an input element and some aesthetic changes

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
-
 document.querySelector("body").appendChild(h2);
+
+const input = document.querySelector('input');
+const log = document.getElementById('values');
+
+input.addEventListener('input', updateValue);
+function updateValue(e) {
+    log.textContent = e.target.value;
+}
+updateValue()
+
