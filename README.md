df234599e710df9d1bba29f90d686b42d6d64675
William <wms274@nyu.edu>
2022-08-21 18:16:24 -0400

    adds an inpirational message, a hint, some text on page1, and fixes functionality and aesthetic issues

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # My first software engineering project
+Guess the same thing twice in a row!
--- script.js
+++ script.js
@@ -2,34 +2,68 @@
 const log = document.getElementById('values');
 const inputArray = []
 
-const textBox = document.querySelector('input');
-textBox.addEventListener("keypress", function(event) {
+function inspiration() {
+    if (inputArray.length === 3) {
+        const para = document.createElement("p");
+        para.textContent = "Keep guessing!";
+        document.querySelector("body").appendChild(para);
+    }
+}
+
+function clickHint() {
+    const para3 = document.createElement("p");
+    para3.textContent = "Your last guess was actually quite good. Very good...";
+    document.querySelector("body").appendChild(para3);
+}
+
+function hint() {
+    if (inputArray.length === 6) {
+        const para2 = document.createElement("p");
+        para2.textContent = "Hint?";
+        document.querySelector("body").appendChild(para2);
+        
+        para2.addEventListener("click", clickHint)
+    }
+}
+
+function inputEnterResponder(event) {
     let input = document.querySelector('input').value;
     if (event.key === "Enter") {
-        if (input.length > 0 && input.length<=5) {
+        if (input.length > 0 && input.length <= 5) {
             log.textContent += `${input}, `;
             inputArray.push(input);
-                if (inputArray[inputArray.length-1]===inputArray[inputArray.length-2] && inputArray.length>1) {
-                    log.textContent = "u got it"
+            document.querySelector('input').value = ""
+                if (inputArray[inputArray.length - 1] === inputArray[inputArray.length - 2] && inputArray.length > 1) {
+                    log.textContent = "You got it! "
                     window.open("./page1.html")
                 }
         } else {
             alert('Guess must contain at least one and no greater than 5 elements');
         }
-    }   
-});
+    inspiration();
+    hint();
+}}
 
-const button = document.querySelector('button');
-button.addEventListener("click", function() {
+function inputButtonResponder() {
     let input = document.querySelector('input').value;
-    if (input.length > 0 && input.length<=5) {
+    if (input.length > 0 && input.length <= 5) {
         log.textContent += `${input}, `;
         inputArray.push(input);
-        if (inputArray[inputArray.length-1]===inputArray[inputArray.length-2] && inputArray.length>1) {
-            log.textContent = "u got it"
+        document.querySelector('input').value = ""
+        if (inputArray[inputArray.length - 1] === inputArray[inputArray.length - 2] && inputArray.length > 1) {
+            log.textContent = "You got it!"
             window.open("./page1.html")
         }
     } else {
         alert('Guess must contain at least one and no greater than 5 elements');
     }
-});
+    inspiration();
+    hint();
+};
+
+
+const textBox = document.querySelector('input');
+textBox.addEventListener("keypress", inputEnterResponder);
+
+const button = document.querySelector('button');
+button.addEventListener("click", inputButtonResponder)
