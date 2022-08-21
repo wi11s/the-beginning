2de1f69d3412c39fff781dc661e247539c1f09bd
William <wms274@nyu.edu>
2022-08-20 22:19:45 -0400

    add second page that opens when password puzzle is solved

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 
 const log = document.getElementById('values');
-//let input = document.querySelector('input').value;
-
-/*const textBox = document.querySelector('input');
-textBox.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    let input = document.querySelector('input').value;
-    log.textContent += `${input}, `
-    alert(input);
-  }
-});
-const button = document.querySelector('button');
-button.addEventListener("click", function() {
-    let input = document.querySelector('input').value;
-    log.textContent += `${input}, `
-    alert(input);
-});*/
+const inputArray = []
 
 const textBox = document.querySelector('input');
 textBox.addEventListener("keypress", function(event) {
@@ -23,16 +8,27 @@ textBox.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         if (input.length > 0 && input.length<=5) {
             log.textContent += `${input}, `;
+            inputArray.push(input);
+                if (inputArray[inputArray.length-1]===inputArray[inputArray.length-2] && inputArray.length>1) {
+                    log.textContent = "u got it"
+                    window.open("./page1.html")
+                }
         } else {
             alert('Guess must contain at least one and no greater than 5 elements');
         }
     }   
 });
+
 const button = document.querySelector('button');
 button.addEventListener("click", function() {
     let input = document.querySelector('input').value;
     if (input.length > 0 && input.length<=5) {
         log.textContent += `${input}, `;
+        inputArray.push(input);
+        if (inputArray[inputArray.length-1]===inputArray[inputArray.length-2] && inputArray.length>1) {
+            log.textContent = "u got it"
+            window.open("./page1.html")
+        }
     } else {
         alert('Guess must contain at least one and no greater than 5 elements');
     }
